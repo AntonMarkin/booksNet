ab3a335ebf6ba809a1bb95ecfc1cae73c73f537d
AntonMarkin <guruzorik@gmail.com>
2023-04-20 11:11:11 +0400

    make comments loading via Ajax
    make script answer form
    
    make page with user comments

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -31,4 +31,42 @@ class Comment extends Model
         return $this->hasMany(Comment::class);
     }
 
+    /**
+     * Get all comments with the status unequal deleted and with the specified parameters limit and offset
+     *
+     * @param integer $profileId
+     * @param integer $limit
+     * @param integer $offset
+     * @return Comment
+     */
+    static function getNotDeletedComments($profileId, $limit = null, $offset = null)
+    {
+        return Comment::where('profile_id', $profileId)
+            ->where('status', '!=', 'deleted')
+            ->limit($limit)
+            ->offset($offset)
+            ->get();
+    }
+
+    /**
+     * Get comments by user id
+     *
+     * @param integer $userId
+     * @return Comment
+     */
+    static function getUserComments($userId)
+    {
+        return Comment::where('user_id', $userId)->get();
+    }
+
+    /**
+     * Finds comment with the status unequal deleted by id and change status to deleted
+     *
+     * @param integer $id
+     * @return Comment
+     */
+    static function deleteComment($id)
+    {
+        return Comment::where('id', $id)->where('status', '!=', 'deleted')->update(['status' => 'deleted']);
+    }
 }
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', '')
+@section('title', 'Profile')
 
 @section('content')
     <div class="container">
@@ -11,45 +11,35 @@
             <h2>Комментарии:</h2>
             @if(Auth::check())
                 <h3>Новый коментарий</h3>
-                <form method="post" action="{{ route('new_comment') }}">
-                    @include('comment_form')
-                </form>
+                @include('comment_form')
             @endif
             <hr>
-            @foreach($comments as $comment)
-                <div>
-                    <div class="card mb-2">
-                        <div class="card-header">
-                            <p id="email">{{$comment->user->email}}</p>
-                        </div>
-                        <div class="card-body">
-                            <h5>{{ $comment->header }}</h5>
-                            <p>{{ $comment->text }}</p>
-                            @if(isset($comment->comment_id))
-                            <hr>
-                                <p class="card-text"><small class="text-body-secondary"><b>Ответ на комментарий: </b>{{ $comment->comment->text }}</small></p>
-                        </div>
-                            @endif
-                        </div>
-                        @if(Auth::check())
-                            <div class="card-footer text-body-secondary">
-                                <a href="#" class="btn btn-outline-dark">Ответить</a>
-
-                                @if(Auth::id() == $comment->profile_id || Auth::id() == $comment->user_id)
-                                    <a href="{{ route('delete_comment', ['id' => $comment->id]) }}" class="btn btn-outline-danger">Удалить</a>
-                                @endif
-                            </div>
-                        <!-- тут отправка данных через ажакс и появление формы ответа на коммент
-                <form method="post" action="{{ route('new_comment') }}">
-                <input type="hidden" name="comment_id" value="{{ $comment->id }}">
-                        include('comment_form')
-</form>-->
-
-                        @endif
-                    </div>
-                </div>
-            @endforeach
-
+            @include('comments')
+            <button class="btn btn-link" id="get-all">Показать все комментарии</button>
+            <div id="comments"></div>
         </div>
-    </div>
+        <script>
+            
+            $(document).on('click', '#get-all', function () {
+                $('#get-all').hide();
+                $.ajax({
+                    url: "/profile/{{ request()->segment(2) }}/all",
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function (data) {
+                        $('#comments').append(data.comments)
+                    }
+                });
+            });
+            function getAnswerForm(id) {
+                $.ajax({
+                    url: "/comment/" + id + "/answer",
+                    type: 'GET',
+                    dataType: 'json',
+                    success: function (data) {
+                        $('#answer_' + id).append(data.answerForm)
+                    }
+                });
+            }
+        </script>
 @endsection
